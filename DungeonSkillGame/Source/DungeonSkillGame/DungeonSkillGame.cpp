c90e07105404ce8a3082bf7f9912a1bfaa02acbe
mike.jurovcik <skylitsoldier13@yahoo.com>
2019-09-06 22:35:08 -0400

    Initial game commit.

---
+++ DungeonSkillGame/Source/DungeonSkillGame/DungeonSkillGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DungeonSkillGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DungeonSkillGame, "DungeonSkillGame" );
